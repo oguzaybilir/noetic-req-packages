dbf7f07ba8ed1fb710084e12c4400f1c900ea10b
Oğuzay Bilir <93535901+oguzaybilir@users.noreply.github.com>
2023-10-26 20:51:01 +0300

    Create get_req.sh

---
+++ get_req.sh
@@ -0,0 +1,30 @@
+sudo apt install -y ros-noetic-rosserial*
+sudo apt install -y ros-noetic-cv*
+sudo apt install -y ros-noetic-joy*
+
+sudo apt install -y ros-noetic-ackermann*
+sudo apt install -y ros-noetic-control*
+sudo apt install -y ros-noetic-joint*
+sudo apt install -y ros-noetic-effort-controllers*
+sudo apt install -y ros-noetic-position-controllers*
+sudo apt install -y ros-noetic-diff-drive-controller*
+sudo apt install -y ros-noetic-hector-gazebo-*
+
+sudo apt install -y ros-noetic-gazebo-*
+
+sudo apt-get install -y ros-noetic-navigation
+sudo apt-get install -y ros-noetic-robot-localization
+sudo apt-get install -y ros-noetic-robot-state-publisher
+
+sudo add-apt-repository ppa:borglab/gtsam-release-4.0 -y
+sudo apt update
+sudo apt install libgtsam-dev libgtsam-unstable-dev -y 
+
+sudo apt-get install libgoogle-glog-dev -y
+sudo apt-get install libeigen3-dev -y 
+sudo apt-get install libpcl-dev -y
+sudo apt-get install libomp-dev -y
+
+sudo apt-get install pcl-tools -y
+sudo apt-get install ros-noetic-pcl-*
+
